09be4334aacd414ddfd2db3026095ac5d7cd3db8
Praval jain <praval41195jain@gmail.com>
2020-04-27 23:17:28 +0530

    add js files for test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import test from "./test"
+import WelcomeMsg from "./welcomeMsg"
+
 document.addEventListener("DOMContentLoaded", function(event) {
     const element = document.createElement('h1')
     element.innerHTML = "Hello World"
     document.body.appendChild(element)
   })
+
+  new WelcomeMsg().sayHello()
+
---
+++ src/welcomeMsg.js
@@ -0,0 +1,5 @@
+export default class WelcomeMsg {
+     sayHello() {
+        alert("Hello, Welcome")
+    }
+}
