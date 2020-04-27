0d704c5f7b7e53d8328760d1bb983479fdcd35cb
Praval jain <praval41195jain@gmail.com>
2020-04-27 22:22:14 +0530

    update package and webapack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path")
+
+module.exports = {
+    entry: [
+        './src/index.js',
+        './src/index.css'
+      ],
+      output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [
+            { 
+              test: /\.css$/, use:[ "style-loader", 'css-loader' ]
+            }
+            ]
+    },
+     devServer: {
+         compress: true,
+         port: 8000
+     }
+}
